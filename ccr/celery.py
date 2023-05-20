4edbac015472dc0831539ac7647509269372476f
twoics <second_vozhzhov@mail.ru>
2023-05-20 22:31:28 +0700

    Init celery email sender by constance time

--- ccr/celery.py
+++ ccr/celery.py
@@ -1,6 +1,8 @@
+import datetime
 import os
 from celery import Celery
 from celery.schedules import crontab
+from constance import config
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccr.settings')
 
@@ -13,6 +15,9 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'send-email-if-available': {
         'task': 'news.tasks.send_email_if_available',
-        'schedule': crontab(),  # Check every minute
+        'schedule': crontab(
+            minute=config.SEND_TIME.minute,
+            hour=config.SEND_TIME.hour
+        ),
     },
 }
